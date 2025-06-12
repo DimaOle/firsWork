6a15ec1e992d1c8c8226208f664caa5e5550608f
Dima Olekseenko <dimkaolekseenko@gmail.com>
2025-06-12 17:25:34 +0300

    change main page

--- blog-ui/src/app/page.tsx
+++ blog-ui/src/app/page.tsx
@@ -1,12 +1,26 @@
+
+//import { useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
+  // useEffect(() => {
+  //   // Пример запроса на backend
+  //   fetch("http://localhost:3001/api/your-endpoint")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log("Backend response:", data);
+  //     })
+  //     .catch((err) => {
+  //       console.error("Error fetching data:", err);
+  //     });
+  // }, []);
+
   return (
     <main className="p-8">
-      <div className="space-x-2">
-        <Link href="/login"><Button>Login</Button></Link>
-        <Link href="/register"><Button>Registration</Button></Link>
+      <div className="fixed top-4 right-4 flex space-x-2">
+        <Link href="/login"><Button size="sm">Sign in</Button></Link>
+        <Link href="/register"><Button size="sm">Sign up</Button></Link>
       </div>
     </main>
   );
--- blog-ui/src/app/register/page.tsx
+++ blog-ui/src/app/register/page.tsx
@@ -1,16 +1,18 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { apiFetch } from "@/lib/api";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
-  const router = useRouter();
+  // const router = useRouter();
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -20,20 +22,39 @@ export default function LoginPage() {
       const res = await apiFetch("/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ email, password, firstName, lastName }),
       });
+
+      const data = await res.json();
+      console.log("Data from backend:", data);
+
       if (res.ok) {
-        router.push("/posts");
+        alert("Registration successful!");
+        // router.push("/posts");
       } else {
-        alert("Login error");
+        alert("Registration error");
       }
     }
-
   };
 
   return (
     <form onSubmit={handleLogin} className="p-8 max-w-md mx-auto">
       <h2 className="text-2xl mb-4">Sign up</h2>
+
+      <Input
+        type="text"
+        placeholder="First Name"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+        className="mb-2"
+      />
+      <Input
+        type="text"
+        placeholder="Last Name"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+        className="mb-2"
+      />
       <Input
         type="email"
         placeholder="Email"
@@ -46,16 +67,17 @@ export default function LoginPage() {
         placeholder="Password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
-        className="mb-4"
+        className="mb-2"
       />
       <Input
-        type="reapetPassword"
-        placeholder="Repeate password"
+        type="password"
+        placeholder="Repeat Password"
         value={repeatPassword}
         onChange={(e) => setRepeatPassword(e.target.value)}
         className="mb-4"
       />
-      <Button type="submit">sign up</Button>
+
+      <Button type="submit">Sign Up</Button>
     </form>
   );
 }
